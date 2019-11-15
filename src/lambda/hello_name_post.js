fd1c545d2b5baf1ccab620cdc9abbdcd5cbc3867
Alex <arosenthal@business.com>
2019-11-15 00:34:45 -0500

    try in_channel to see if it can be visible

--- src/lambda/hello_name_post.js
+++ src/lambda/hello_name_post.js
@@ -9,10 +9,11 @@ exports.handler = async (event, context) => {
   // When the method is POST, the name will no longer be in the event’s
   // queryStringParameters – it’ll be in the event body encoded as a query string
   const params = querystring.parse(event.body);
-  const name = params.text || "World";
+  const name = params.text || "error";
 
   return {
     statusCode: 200,
+    response_type: 'in_channel',
     body: `${name} ${name} ${name} ${name} ${name} ${name} ${name}`
   };
 };
